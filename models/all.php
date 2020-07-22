27c786ff2f68494c65c8d45c1475ff9543966904
enrique <user23qiqe@gmail.com>
2020-07-22 14:12:27 -0500

    Agrega archivo contenedor para ocultar la conexion a base de datos

---
+++ models/all.php
@@ -0,0 +1,8 @@
+<?php
+
+/*
+    Con esta parte se trata de ocultar cual es el archivo contenedor de la 
+    BD, Donde ahora todos los models realizaran la referencia correspondiente
+*/
+    include_once 'db.php';
+?>
